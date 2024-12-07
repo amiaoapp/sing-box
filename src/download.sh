2206e8c617419793d2070fa6e6ce92f55dd35e82
amiao <xiaolv516@gmail.com>
2024-12-07 20:28:58 +0800

    Update download.sh
    https://ghp.ci/

--- src/download.sh
+++ src/download.sh
@@ -2,15 +2,15 @@ get_latest_version() {
     case $1 in
     core)
         name=$is_core_name
-        url="https://api.github.com/repos/${is_core_repo}/releases/latest?v=$RANDOM"
+        url="https://ghp.ci/https://api.github.com/repos/${is_core_repo}/releases/latest?v=$RANDOM"
         ;;
     sh)
         name="$is_core_name 脚本"
-        url="https://api.github.com/repos/$is_sh_repo/releases/latest?v=$RANDOM"
+        url="https://ghp.ci/https://api.github.com/repos/$is_sh_repo/releases/latest?v=$RANDOM"
         ;;
     caddy)
         name="Caddy"
-        url="https://api.github.com/repos/$is_caddy_repo/releases/latest?v=$RANDOM"
+        url="https://ghp.ci/https://api.github.com/repos/$is_caddy_repo/releases/latest?v=$RANDOM"
         ;;
     esac
     latest_ver=$(_wget -qO- $url | grep tag_name | egrep -o 'v([0-9.]+)')
@@ -32,7 +32,7 @@ download() {
     core)
         name=$is_core_name
         tmpfile=$tmpdir/$is_core.tar.gz
-        link="https://github.com/${is_core_repo}/releases/download/${latest_ver}/${is_core}-${latest_ver:1}-linux-${is_arch}.tar.gz"
+        link="https://ghp.ci/https://github.com/${is_core_repo}/releases/download/${latest_ver}/${is_core}-${latest_ver:1}-linux-${is_arch}.tar.gz"
         download_file
         tar zxf $tmpfile --strip-components 1 -C $is_core_dir/bin
         chmod +x $is_core_bin
@@ -40,7 +40,7 @@ download() {
     sh)
         name="$is_core_name 脚本"
         tmpfile=$tmpdir/sh.tar.gz
-        link="https://github.com/${is_sh_repo}/releases/download/${latest_ver}/code.tar.gz"
+        link="https://ghp.ci/https://github.com/${is_sh_repo}/releases/download/${latest_ver}/code.tar.gz"
         download_file
         tar zxf $tmpfile -C $is_sh_dir
         chmod +x $is_sh_bin ${is_sh_bin/$is_core/sb}
@@ -49,7 +49,7 @@ download() {
         name="Caddy"
         tmpfile=$tmpdir/caddy.tar.gz
         # https://github.com/caddyserver/caddy/releases/download/v2.6.4/caddy_2.6.4_linux_amd64.tar.gz
-        link="https://github.com/${is_caddy_repo}/releases/download/${latest_ver}/caddy_${latest_ver:1}_linux_${is_arch}.tar.gz"
+        link="https://ghp.ci/https://github.com/${is_caddy_repo}/releases/download/${latest_ver}/caddy_${latest_ver:1}_linux_${is_arch}.tar.gz"
         download_file
         tar zxf $tmpfile -C $tmpdir
         cp -f $tmpdir/caddy $is_caddy_bin
